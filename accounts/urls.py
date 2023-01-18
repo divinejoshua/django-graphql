2c898106d817bd706e4c4b7578d2c1f866388498
divinejoshua <divineuche399@gmail.com>
2023-01-18 13:59:08 +0000

    Made changes

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+app_name = "accounts_api"
+
+urlpatterns = [
+    path('user/', views.check_user.as_view(), name='get_user'),                       #Check user api
+    path('auth/', include('dj_rest_auth.urls', namespace='djrest_api')),                #dj rest auth login, change password etc
+
+]
