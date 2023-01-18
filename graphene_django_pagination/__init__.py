d78a53acfde9413da90ab255707dfb5d6a11500b
divinejoshua <divineuche399@gmail.com>
2023-01-18 15:55:52 +0000

    deleted package

--- graphene_django_pagination/__init__.py
+++
@@ -1,9 +0,0 @@
-from .objects_type import PageInfoExtra
-from .connection import PaginationConnection
-from .connection_field import DjangoPaginationConnectionField
-
-__all__ = [
-    "PageInfoExtra",
-    "PaginationConnection",
-    "DjangoPaginationConnectionField"
-]
