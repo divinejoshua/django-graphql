8769293a5a9c502e659c4f7df8359e7b058b7842
divinejoshua <divineuche399@gmail.com>
2023-01-18 13:34:04 +0000

    Completed basic graphql

--- ingredients/urls.py
+++ ingredients/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
 
@@ -8,5 +8,5 @@ from graphene_django.views import GraphQLView
 from .schema import schema
 
 urlpatterns = [
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path("graphql", GraphQLView.as_view(graphiql=True, schema=schema)),
 ]
